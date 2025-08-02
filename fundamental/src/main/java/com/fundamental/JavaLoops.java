f036b30d22a30a3896f3bac9fe1bbffe033d617c
Reza Bagus Saputra <103319101+RezaBagusS@users.noreply.github.com>
2025-08-03 01:10:42 +0700

    recall : Loops on java program

---
+++ fundamental/src/main/java/com/fundamental/JavaLoops.java
@@ -0,0 +1,23 @@
+package com.fundamental;
+
+public class JavaLoops {
+    
+    public static void main(String[] args) {
+        
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        int l = 0;
+        while (10 > l) {
+            System.out.println(l);
+        }
+        
+        int doWhile = 0;
+        do {
+            System.out.println(doWhile);
+        } while (10 > doWhile);
+
+    }
+
+}
